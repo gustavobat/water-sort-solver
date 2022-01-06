db8bc577d316c13bbaebb9b1eb755c005e4cb28a
gustavobat <gustavoabatistela@gmail.com>
2022-01-06 13:20:25 -0300

    Add initial file

---
+++ water-sort-solver.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+# Rules:
+# - Only the top ball of each stack can be moved.
+# - A ball can be moved to the top of another ball of the same colour
+# - A ball can be moved to an empty stack.
+
