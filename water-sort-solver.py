b0756cf73c8049025b7a0db82d8f0fc6344ebfba
gustavobat <gustavoabatistela@gmail.com>
2022-01-06 13:22:10 -0300

    Add Color Enum class

--- water-sort-solver.py
+++ water-sort-solver.py
@@ -1,7 +1,26 @@
 from enum import Enum
 
+
 # Rules:
 # - Only the top ball of each stack can be moved.
 # - A ball can be moved to the top of another ball of the same colour
 # - A ball can be moved to an empty stack.
+
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+    YELLOW = 4
+    WHITE = 5
+    GREY = 6
+    PINK = 7
+    DARK_RED = 8
+    DARK_GREEN = 9
+    DARK_BLUE = 10
+    DARK_YELLOW = 11
+    LIGHT_RED = 12
+    LIGHT_GREEN = 13
+    LIGHT_BLUE = 14
+    LIGHT_YELLOW = 15
 
